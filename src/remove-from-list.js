63c1db5aff34fdf5bfdcea54fecefa2577b36fba
playoffthecuff <jmobile@bk.ru>
2025-02-06 08:57:14 +0300

    feat: add remove from list

--- src/remove-from-list.js
+++ src/remove-from-list.js
@@ -1,6 +1,6 @@
 const { NotImplementedError } = require('../extensions/index.js');
 
-// const { ListNode } = require('../extensions/list-node.js');
+const { ListNode } = require('../extensions/list-node.js');
 
 /**
  * Given a singly linked list of integers l and an integer k,
@@ -22,9 +22,27 @@ const { NotImplementedError } = require('../extensions/index.js');
  *   }
  * }
  */
-function removeKFromList(/* l, k */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function removeKFromList(l, k) {
+  let p = null;
+  let c = l;
+  while (c) {
+    if (c.value === k) {
+      if (!c.next) {
+        p.next = null;
+        c = null;   
+      } else if (!p) {
+        l = l.next;
+        c = l;
+      } else {
+        p.next = c.next;
+        c = c.next;
+      }
+    } else {
+      p = c;
+      c = c.next;
+    }
+  }
+  return l;
 }
 
 module.exports = {
